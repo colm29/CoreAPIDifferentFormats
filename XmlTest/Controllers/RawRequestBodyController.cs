9f6116166c965c7b032f7ac59340ae30d68232d7
colm29 <colm29@gmail.com>
2019-09-04 17:53:13 +0100

    Initial Commit

---
+++ XmlTest/Controllers/RawRequestBodyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XmlTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RawRequestBodyController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Please post to api/RawRequestBody to test receiving Raw Request Body";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<ContentResult> Post([FromBody] string body)
+        {
+            var requestData = body;
+
+            var response = new ContentResult
+            {
+                Content = requestData,
+                StatusCode = 200
+            };
+            return await Task.FromResult(response);
+        }
+    }
+}
